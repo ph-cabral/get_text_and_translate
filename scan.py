6ddfac8cd5d8954d8e2ee9830846408333e4a169
pablo <pabloc.090.pc@gmail.com>
2023-03-24 14:21:14 -0300

    edit sacan.py

--- scan.py
+++ scan.py
@@ -2,29 +2,31 @@ import cv2
 import easyocr
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 target_language = "es-ES"
 
-reader = easyocr.Reader(["pt", "en"], gpu=False)
+reader = easyocr.Reader(["pt"], gpu=False)
 
 image = cv2.imread("./imagen.jpeg")
-result = reader.readtext(image, paragraph=True, detail=0)
+result = reader.readtext(image, paragraph=True)
 
 for res in result:
     
-    text = " ".join(res)
-    # text = text.replace("\n", " ")
-    # text = text.replace(" -  ", "")
-    # text = text.replace("  ", " ")
-    # text = text.strip()
-    text = text.replace(' ', '').replace('-', '').replace('  ', ' ').strip()
+    text = " ".join(res[1])
+    text = text.replace("\n", " ")
+    text = text.replace("   ", "+")
+    text = text.replace(" -+", "")
+    text = text.replace(" ", "")
+    text = text.replace("+", " ")
+    text = text.lower()
+    text = text.strip()
 
 
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=(f"Traduce el siguiente texto a {target_language}: \"{text}\""),
-        max_tokens=60,
+        max_tokens=30,
     )
 
     translation = response.choices[0].text.strip()
@@ -32,4 +34,4 @@ for res in result:
     print(text)
     print(translation)
     print('*******************************************************')
-  
+ 
