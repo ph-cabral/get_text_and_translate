bfe8fed91e665dd44f72a6df216eb58a2f858cb4
pablo <pabloc.090.pc@gmail.com>
2023-03-27 09:15:20 -0300

    testing with jp

--- scan.py
+++ scan.py
@@ -1,16 +1,25 @@
 import cv2
 import easyocr
 import openai
+import os 
 
-openai.api_key = "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"
+carpeta = "traducciones"
+archivo = "japo.txt"
+ruta_archivo = os.path.join(carpeta, archivo)
+
+openai.api_key = apikey
 
 target_language = "es-ES"
 
-reader = easyocr.Reader(["pt"], gpu=False)
+reader = easyocr.Reader(["ja"], gpu=False)
 
-image = cv2.imread("./imagen.jpeg")
+
+image = cv2.imread("./1.jpg")
+print(1)
 result = reader.readtext(image, paragraph=True)
 
+
+print(result)
 for res in result:
     
     text = " ".join(res[1])
@@ -22,16 +31,22 @@ for res in result:
     text = text.lower()
     text = text.strip()
 
+    with open(ruta_archivo, mode="a") as archivo:
+        archivo.write(f"***********************\n{text}")
 
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=(f"Traduce el siguiente texto a {target_language}: \"{text}\""),
-        max_tokens=30,
-    )
+    trad = {'t1':'', 't2':'', 't3':''}
+    for l in trad.keys():
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=(f"Traduce el siguiente texto a {target_language}: \"{text}\""),
+            max_tokens=30,
+        )
+        trad[l] = response.choices[0].text.strip()
+    
+    with open(ruta_archivo, mode="a") as archivo:
+        archivo.write(f"\n{trad['t1']}\n{trad['t2']}\n{trad['t3']}\n\n\n")
+print(1)
 
-    translation = response.choices[0].text.strip()
+# os.mkdir(carpeta)
 
-    print(text)
-    print(translation)
-    print('*******************************************************')
- 
+
